cdd5341d200db65b2a75c1eb8304faf765568ee1
Saurabh Patel <saurabhpatel230@hotmail.com>
2024-06-14 01:46:54 +0530

    feat: adds config file to get env vars

---
+++ src/configs/index.js
@@ -0,0 +1,7 @@
+// Mapper for environment variables
+export const environment = process.env.NODE_ENV;
+export const port = process.env.PORT;
+
+export const logDirectory = process.env.LOG_DIR;
+
+export const dbURL = process.env.DATABASE_URL;
