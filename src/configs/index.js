2345fb74b38ce71f81320895c4fa8bb143e1f33d
Saurabh Patel <saurabhpatel230@hotmail.com>
2024-06-14 10:41:15 +0530

    feat(env): adds jwt & email env variables

--- src/configs/index.js
+++ src/configs/index.js
@@ -5,3 +5,10 @@ export const port = process.env.PORT;
 export const logDirectory = process.env.LOG_DIR;
 
 export const dbURL = process.env.DATABASE_URL;
+
+export const jwtSecret = process.env.JWT_SECRET;
+export const jwtExpiresIn = process.env.JWT_EXPIREIN;
+
+export const emailService = process.env.SMTP_SERVICE;
+export const emailUser = process.env.SMTP_USER;
+export const emailPassword = process.env.SMTP_PASS;
