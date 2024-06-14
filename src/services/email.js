9a9ab193ff6b2327194e713fc00eaae639424ca4
Saurabh Patel <saurabhpatel230@hotmail.com>
2024-06-14 10:44:02 +0530

    feat(email): adds email sender function

---
+++ src/services/email.js
@@ -0,0 +1,15 @@
+import nodemailer from 'nodemailer';
+import { emailPassword, emailService, emailUser } from 'configs';
+
+const transport = nodemailer.createTransport({
+  service: emailService,
+  auth: {
+    user: emailUser,
+    pass: emailPassword
+  },
+});
+
+export const sendEmail = async (to, subject, body) => {
+  let message = { from: `${emailUser} TODOs App`, to, subject, body };
+  return await transport.sendMail(message);
+};
